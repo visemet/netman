5b24acdc7755a2ffe393d1bcd35868422c60e9f9
Max Hirschhorn <maxh@caltech.edu>
2012-10-24 23:02:53 -0700

    Updated formatting.

--- src/device.py
+++ src/device.py
@@ -8,18 +8,18 @@ class Device:
         Initializes the device. Implemented in each subclass.
         """
 
-        raise NotImplementedError('Device.initialize()')
+        raise NotImplementedError, 'Device.initialize()'
 
     def send(self, packet):
         """
         Sends the specified packet. Implemented in each subclass.
         """
 
-        raise NotImplementedError('Device.send(packet)');
+        raise NotImplementedError, 'Device.send(packet)'
 
     def process(self, event):
         """
         Processes the specified event. Implemented in each subclass.
         """
         
-        raise NotImplementedError('Device.process(event)')
+        raise NotImplementedError, 'Device.process(event)'
