8769906950d095a845bd614980a821ad5874fcb4
Max Hirschhorn <maxh@caltech.edu>
2012-11-13 19:15:12 -0800

    Added type safety to devices argument in constructor.
    Ensures that the specified devices is a list of Device instances. Also
    updated the documentation of functions within the Simulation class.

--- src/simulation.py
+++ src/simulation.py
@@ -1,25 +1,38 @@
+from heapq import heappush, heappop
+
 from device import Device
 from event import Event
-from heapq import heappush, heappop
 
 class Simulation:
     """
+    Class for running multi‐link, multi‐flow network simulations.
     """
 
     def __init__(self, devices):
         """
-        Creates a simulation instance with the specified list of
+        Creates a Simulation instance with the specified list of
         devices.
         """
 
-        # TODO: check devices is list of Device instances
+        # Checks that devices is a list
+        if not isinstance(devices, list):
+            raise TypeError, 'devices must be a list'
+
+        # Iterates through each device
+        for device in devices:
+            # Checks that each device is a Device instance
+            if not isinstance(device, list):
+                raise TypeError, 'device must be a Device instance'
+
+        # Stores the list of devices
         self._devices = devices
 
+        # Initializes an empty event queue
         self._event_queue = []
 
     def _initialize(self):
         """
-        Initializes the simulation.
+        Initializes the simulation by initializing each device.
         """
 
         # Iterates through each device
@@ -27,12 +40,13 @@ class Simulation:
             # Initializes each device
             events = device.initialize()
 
+            # Pushes each event onto the queue
             for event in events:
                 heappush(self._event_queue, event)
 
     def start(self):
         """
-        Starts the simulation.
+        Starts the simulation and executes it until completion.
         """
 
         # Initializes the simulation
@@ -40,15 +54,17 @@ class Simulation:
         
         # Loops through all events on the queue
         while self._event_queue:
-            # Removes the event from the head of the buffer
+            # Pops the head off of the event queue
             event = heappop(self._event_queue)
             device = event.port().device()
 
             # Processes the event
             spawned_events = device.process(event)
-            # Adds the spawned event to the queue
+            # Pushes each spawned event onto the queue
             for spawned_event in spawned_events:
                 heappush(self._event_queue, spawned_event)
+
+        # TODO: generate graphs from the statistics
 
         for device in self._devices:
             print (device, device._algorithm._routing_table)
